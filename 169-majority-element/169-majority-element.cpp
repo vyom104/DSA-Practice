0c99bcc409cc3ec01cf05d9bf3505877cb1b5d74
VYOM GUPTA <55016130+vyom104@users.noreply.github.com>
2022-06-02 11:07:37 +0530

    Time: 41 ms (10.42%), Space: 19.7 MB (14.20%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,13 +5,14 @@ public:
         int max=INT_MIN,maxEle;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
+            if(mp[nums[i]]>nums.size()/2) maxEle=nums[i];
         }
-            for(auto it:mp){
-                if(it.second>max){
-                    max=it.second;
-                    maxEle=it.first;
-                }
-            }
+            // for(auto it:mp){
+            //     if(it.second>max){
+            //         max=it.second;
+            //         maxEle=it.first;
+            //     }
+            // }
         return maxEle;
     }
 };
